dcec93ce5cb859d694f8224d374fbb8575f027ac
sandreae <git@contact.samandreae.com>
2021-04-08 11:59:59 +0100

    Create and use default config file

--- app.js
+++ app.js
@@ -8,12 +8,13 @@ var index = require('./routes/index');
 var events = require('./routes/events');
 var documentation = require('./routes/documentation');
 
+var config = require('config').get('Site');
 var port = process.env.PORT || 3000
 var promise = mongoose.connect(process.env.MONGO_URL  ||  'mongodb://mongo:27017/klangkeller');
 
-const TITLE="s10c"
-const VENUES=["kino", "keller", "hof"]
-const MEMBERS=[{name: "klaas", email: "klaas[at]klangkeller.net"},{name: "mio", email: "mio[at]klangkeller.net"},{name: "aziz", email: "aziz[at]klangkeller.net"},{name: "sam", email: "sam[at]klangkeller.net"},,{name: "chris", email: "chris[at]klangkeller.net"},]
+const TITLE=config.get('title');
+const VENUES=config.get('venues');
+const MEMBERS=config.get('organisers');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
