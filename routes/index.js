d57cd1ead5a73a2de88321d7096828f9f866e5bd
sandreae <git@contact.samandreae.com>
2021-04-13 09:34:43 +0100

    Add route forwarding for signup and admin

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,12 @@ var events = require('../controllers/EventController.js');
 // Get all events
 router.get('/', events.getAll, events.processEvents, events.showAll);
 
+// Forwarders for routes that need query params
+router.get('/signup', function (req, res, next) {
+  res.redirect('/events/?signup=true');
+});
+router.get('/admin', function (req, res, next) {
+  res.redirect('/events/?admin=true');
+});
+
 module.exports = router;
